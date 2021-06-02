36d6bf79e927507bc1a7f30acfa254746da6bcd9
warlinux <warlinux@yandex.ru>
2021-06-02 17:32:42 +0300

    отрисовка яблока

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ body_snake = [(x, y)]
 length_snake = 1
 dx, dy = 0, 0
 fps = 7
+apple = randrange(0, WINDOW_WIDTH, OBJECT_SIZE), randrange(0, WINDOW_HEIGHT, OBJECT_SIZE)
 # Словарь движения
 traffic_dict = {"W": (0, -1), "S": (0,1), "A": (-1,0), "D": (1,0)}
 # Цикл программы
@@ -22,11 +23,18 @@ while True:
     # Отрисовка змейки
     for i, j in body_snake:
         pygame.draw.rect(screen, pygame.Color('green'), (i, j, OBJECT_SIZE, OBJECT_SIZE))
+    # Отрисовка яблока
+    pygame.draw.rect(screen, pygame.Color('red'), (*apple, OBJECT_SIZE, OBJECT_SIZE))
     # Изменение координат змейки
     x += dx * OBJECT_SIZE
     y += dy * OBJECT_SIZE
     body_snake.append((x, y))
     body_snake = body_snake[-length_snake:]
+    # Поедание яблока
+    if body_snake[-1] == apple:
+        apple = randrange(0, WINDOW_WIDTH, OBJECT_SIZE), randrange(0, WINDOW_HEIGHT, OBJECT_SIZE)
+        length_snake += 1
+        fps += 1
     # Модуль для индетифицирования нажатой клавиши
     key = pygame.key.get_pressed()
     # Условия движения
