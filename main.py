9d0b3278bcd554b15c0455d0b626c63752f2cbf7
warlinux <warlinux@yandex.ru>
2021-06-02 17:20:29 +0300

    Snake commit

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ body_snake = [(x, y)]
 length_snake = 1
 dx, dy = 0, 0
 fps = 7
+# Словарь движения
+traffic_dict = {"W": (0, -1), "S": (0,1), "A": (-1,0), "D": (1,0)}
 # Цикл программы
 while True:
     # Показ экрана и закраска его в черный цвет
@@ -25,6 +27,17 @@ while True:
     y += dy * OBJECT_SIZE
     body_snake.append((x, y))
     body_snake = body_snake[-length_snake:]
+    # Модуль для индетифицирования нажатой клавиши
+    key = pygame.key.get_pressed()
+    # Условия движения
+    if key[pygame.K_w] and (dx, dy) != traffic_dict["S"]:
+        dx, dy = traffic_dict["W"]
+    if key[pygame.K_s] and (dx, dy) != traffic_dict["W"]:
+        dx, dy = traffic_dict["S"]
+    if key[pygame.K_a] and (dx, dy) != traffic_dict["D"]:
+        dx, dy = traffic_dict["A"]
+    if key[pygame.K_d] and (dx, dy) != traffic_dict["A"]:
+        dx, dy = traffic_dict["D"]
     # Условие закрытия программы
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -33,3 +46,5 @@ while True:
     pygame.display.flip()
     # Управление частотой кадров
     clock = pygame.time.Clock()
+    # Количество раз выполнения цикла в секунду
+    clock.tick(2*fps)
